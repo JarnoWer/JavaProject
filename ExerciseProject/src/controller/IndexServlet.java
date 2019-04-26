8ea8bd85204e3598afb71554fe2694975bc770a0
Jarno Wermundsen <43230179+JarnoWer@users.noreply.github.com>
2019-04-26 13:10:51 +0300

    Start files again

--- ExerciseProject/src/controller/IndexServlet.java
+++ ExerciseProject/src/controller/IndexServlet.java
@@ -1,41 +1,32 @@
 package controller;
 
 import java.io.IOException;
+import java.util.List;
+
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
-/**
- * Servlet implementation class IndexServlet
- */
-@WebServlet("/IndexServlet")
+import database.dao.ArtistDao;
+import model.Artist;
+
+@WebServlet("/index")
 public class IndexServlet extends HttpServlet {
+
 	private static final long serialVersionUID = 1L;
-       
-    /**
-     * @see HttpServlet#HttpServlet()
-     */
-    public IndexServlet() {
-        super();
-        // TODO Auto-generated constructor stub
+	private ArtistDao dao = new ArtistDao();
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        resp.addHeader("Content-Type", "text/plain");
+
+        List<Artist> allArtists = dao.getAllArtists();
+
+        // Tulostetaan HTTP-vastaukseen
+        for (Artist a : allArtists) {
+            resp.getWriter().println(a.getId() + ": " + a.getName());
+        }
     }
-
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		response.getWriter().append("Served at: ").append(request.getContextPath());
-	}
-
-	/**
-	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
-	 */
-	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
-		doGet(request, response);
-	}
-
 }
--- ExerciseProject/src/database/dao/ArtistDaoTest.java
+++ ExerciseProject/src/database/dao/ArtistDaoTest.java
@@ -1,14 +1,37 @@
 package database.dao;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-import org.junit.jupiter.api.Test;
+import java.util.List;
 
-class ArtistDaoTest {
+import org.junit.Test;
 
-	@Test
-	void test() {
-		fail("Not yet implemented");
-	}
+import model.Artist;
 
+public class ArtistDaoTest {
+
+    @Test
+    public void artistWithId50IsMetallica() {
+        ArtistDao dao = new ArtistDao();
+        Artist artist = dao.getArtist(50);
+
+        assertEquals("Metallica", artist.getName());
+    }
+
+    @Test
+    public void getArtistWithUnknownIdReturnsNull() {
+        ArtistDao dao = new ArtistDao();
+        Artist artist = dao.getArtist(-100);
+
+        assertEquals(null, artist);
+    }
+
+    @Test
+    public void getAllArtistsReturnsArtistsWithCorrectNames() {
+        ArtistDao dao = new ArtistDao();
+        List<Artist> allArtists = dao.getAllArtists();
+
+        assertEquals("A Cor Do Som", allArtists.get(0).getName());
+        assertEquals("Zeca Pagodinho", allArtists.get(allArtists.size() - 1).getName());
+    }
 }
